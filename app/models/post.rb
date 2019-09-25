92d0372c42318fa026de1886821b0852169f1caf
J-B Garway-Templeman <jbgarwayt@gmail.com>
2019-09-25 16:54:54 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, {presence: true}
+    validates :content, {length: {minimum: 250}}
+    validates :summary, {length: {maximum: 250}}
+    validates :category, {inclusion: {in: ["Fiction", "Non-Fiction"]}}
+    validate :click_bait?
+
+    CLICKBAIT_PATTERNS = [/Won't Believe/i,/Secret/i,/Top [0-9]*/i,/Guess/i]
+
+    def click_bait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
